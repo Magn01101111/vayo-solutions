63d86b24d23ab51b5649460364b3664bc22e45ad
Jorge Escobar <jescobar@subcargo.com>
2026-04-02 22:35:23 -0300

    carrito funcional +bonito

--- src/app/shared/components/public/public-header/public-header.component.ts
+++ src/app/shared/components/public/public-header/public-header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { QuotationService } from '../../../../core/services/quotation.service';
+import { CartDropdownComponent } from "../cart-dropdown/cart-dropdown.component";
 
 @Component({
   selector: 'app-public-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CartDropdownComponent],
   templateUrl: './public-header.component.html',
   styleUrl: './public-header.component.scss',
 })
